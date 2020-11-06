82fb692443a371945cd17afeccf0f23d3bbd4429
ninyhorlah <58943705+ninyhorlah@users.noreply.github.com>
2020-11-06 06:21:36 +0100

    Big chart template created

--- src/Components/Bigchart.js
+++ src/Components/Bigchart.js
@@ -4,13 +4,13 @@ import { Line } from 'react-chartjs-2';
 const Bigchart = () => {
 
     const state = {
-        labels: ['', '', '', '', ''],
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
         datasets: [
             {
                lineTension: 0.2,
                 borderWidth: 1,
                 borderColor: '#0294FF',
-                data: [27, 26, 28, 27, 30]
+                data: [[57, 60, 88], [66, 50, 100], [58, 99, 44], [57, 102, 99], [60, 90, 101]]
             }
         ]
     }
